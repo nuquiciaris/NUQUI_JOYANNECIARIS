db92b5d21fb6455c53a20ce415ea87b476e9e27d
nuquiciaris <jbnuqui@fit.edu.ph>
2026-04-20 11:56:10 +0800

    4/20/26 11:51: made changes to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Activities</title>
+</head>
+<body>
+    <div class="header">
+        <h1>These are my PHP activities</h1>
+        <p>PHP is fun</p>
+    </div>
+</body>
+</html>
