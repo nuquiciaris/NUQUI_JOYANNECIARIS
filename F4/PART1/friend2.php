6ac3d52844eec539d5febfee587c1a362da9a8bb
nuquiciaris <jbnuqui@fit.edu.ph>
2026-05-18 21:27:49 +0800

    Add files via upload

---
+++ F4/PART1/friend2.php
@@ -0,0 +1,33 @@
+
+<img src="3.jpg" class="friend-img">
+
+<p>
+    Abigael Santiago was born on July 28, 2005, and has
+     lived in both Caloocan and Quezon City, often switching homes 
+     depending on the occasion. We first met during our senior high 
+     school years at the University of the East, though later on, 
+     she moved to South Korea. Abby is hardworking in academics but, 
+     like Aaron, can easily become overwhelmed by school responsibilities. 
+     She enjoys playing Genshin Impact, watching anime, and listening to K-pop, 
+     especially Enhypen. One of the memorable things about our friendship is attending
+      concerts together, which reflects our shared interests in music and entertainment. 
+      Even with the challenges of balancing studies and personal life, Abby continues to 
+      persevere and remain passionate about the things she enjoys, since I catch up with
+       her from time to time about her life in South Korea.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</P>
