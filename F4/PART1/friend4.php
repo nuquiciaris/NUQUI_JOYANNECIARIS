e6951ecb74ee9658ec1a8c24a0854026fda7150f
nuquiciaris <jbnuqui@fit.edu.ph>
2026-05-18 21:29:57 +0800

    Add files via upload

---
+++ F4/PART1/friend4.php
@@ -0,0 +1,17 @@
+<img src="5.jpg" class="friend-img">
+
+
+<p>
+   Kristina Casandra Olaco was born on October 10, 2005, and is from Caloocan. 
+   During our senior high school years at the University of the East, KC was one of 
+   my remaining classmates in Grade 12. At first, KC appeared shy and quiet, but over time,
+    she became our class president, showing leadership and responsibility 
+    that many people did not initially expect. Despite her position, 
+    she remained humble and approachable toward everyone in class. 
+    KC was also very hardworking academically and always came prepared,
+     even bringing printed reviewers to school to help herself and others study better. 
+     Her dedication to academics and leadership responsibilities made her 
+     someone whom classmates could rely on and respect. 
+
+
+</p>
