e45133681c3623a0f03bb633170427680792400e
nuquiciaris <jbnuqui@fit.edu.ph>
2026-05-18 20:50:56 +0800

    Add files via upload

---
+++ F4/PART1/friend1.php
@@ -0,0 +1,11 @@
+<p>David Aaron Santos was born on March 25, 2006, and is from Malabon. 
+    During our senior high school years at the University of the East, 
+    Aaron was known as someone who balanced academics and hobbies well despite 
+    often becoming overwhelmed from pressure. Although we were no longer classmates in 
+    Grade 12 because he belonged to another section, we still remained close and often 
+    studied together. He enjoys playing Genshin Impact and Honkai Star Rail,
+     and he is especially dedicated when it comes to grinding accounts, 
+     even helping with mine. Outside academics and gaming, Aaron also enjoys 
+     anime and listens to K-pop groups such as BLACKPINK and BTS. Despite his 
+     tendency to get stressed easily, he continues to work hard and remains dependable 
+     to the people around him. </p>
---
+++ F4/PART1/friend4.php
@@ -0,0 +1,13 @@
+<p>
+    Mariyel Pacleb was born on October 7, 2005, and is from Caloocan. 
+    We met during our senior high school years at the University of the East, 
+    although in Grade 12, she and Aaron belonged to another section. 
+    Mariyel is known for being outgoing and approachable,
+     making it easy for others to talk to her. One of the things I admire most about 
+     her is how open-minded she is toward different beliefs and perspectives despite her
+      religion. Although she often became ill and had absences because of her health,
+       she still remained hardworking and committed to both schoolwork and responsibilities. 
+       Her ability to continue studying and working hard despite personal 
+       difficulties reflects her resilience and determination. 
+
+</p>
