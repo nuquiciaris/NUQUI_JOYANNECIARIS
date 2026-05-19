abb037cc7700b83d318c2627cd7875e27f2c9d13
nuquiciaris <jbnuqui@fit.edu.ph>
2026-05-19 10:38:49 +0800

    Delete TSA2/PART3 directory

--- TSA2/PART3/objective.php
+++
@@ -1,6 +0,0 @@
-<h3>Career Objective</h3>
-<p>
-    To become a successful Information Technology professional by applying my knowledge
-    and skills in programming and web development, while continuously learning and 
-    improving in a dynamic work environment.
-</p>
