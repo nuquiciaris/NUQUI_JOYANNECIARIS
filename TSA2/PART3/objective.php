7b7216df593a2877610c1286cf1ec040cacf2bd0
nuquiciaris <jbnuqui@fit.edu.ph>
2026-05-19 10:24:50 +0800

    Add files via upload

---
+++ TSA2/PART3/objective.php
@@ -0,0 +1,6 @@
+<h3>Career Objective</h3>
+<p>
+    To become a successful Information Technology professional by applying my knowledge
+    and skills in programming and web development, while continuously learning and 
+    improving in a dynamic work environment.
+</p>
